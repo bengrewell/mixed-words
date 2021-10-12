d9661c8955d02b6fd25bab26c0ecf7dbd5c08b45
Ben Grewell <benjamin.grewell@intel.com>
2021-10-12 08:59:23 -0700

    add timing code

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 # This contains the basic plumbing to help you do the challenge without having to worry about the unimportant
 # parts of the challenge. Feel free to modify any of this code as you see fit.
+import time
+
 import initialization
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
+    # Start skeleton code
     words, scrambled = initialization.init()
     print('[+] Init complete!')
+    start_time = time.time()
 
-    # Do you solution here or in functions above as needed
+    # === Do you solution here or in functions above as needed ===
 
-
+    # Ending skeleton code
+    print(f'[!] Execution time {time.time() - start_time} seconds')
